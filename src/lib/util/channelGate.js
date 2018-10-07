b672bdc0fec68f8f5964813af93f1e7f3af0b55d
Juan <abrecan.eyndrill@gmail.com>
2018-10-07 16:44:47 -0400

    'done'

---
+++ src/lib/util/channelGate.js
@@ -0,0 +1,26 @@
+const RECORD = {};
+const HOUR_DIFF = 3600000;
+
+export default class ChannelGate {
+    async handle(message, isStaff) {
+        const {channel, createdAt, member} = message;
+
+        if (RECORD[member.id] === undefined) {
+            RECORD[member.id] = createdAt.getTime();
+
+            if (isStaff) {
+                console.log('Yes hello there pal')
+            }
+        } else {
+            const lastMessage = RECORD[member.id];
+            const curMessage  = createdAt.getTime();
+            const diff        = Math.abs(curMessage - lastMessage) / HOUR_DIFF;
+
+            if (diff < 24) {
+                return await message.delete();
+            } else {
+                record[member.id] = curMessage;
+            }
+        }
+    }
+}
